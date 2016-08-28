c80cef29558c690a1fc8c1d1bfb4d9cbec13baed
Ramachandra Junior <d.ajaykumar.017@gmail.com>
2016-08-28 16:21:48 +0530

    Initial commit

---
+++ assets/js/todos.js
@@ -0,0 +1,25 @@
+// Check off Specific Todos by clicking
+$("ul").on("click", "li", function () {
+	$(this).toggleClass("completed");
+});
+
+// Click on X to delete Todos
+$("ul").on("click", "span",function (event) {
+	$(this).parent().fadeOut(500, function () {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+// When enter is pressed then 
+$('input[type="text"]').keypress(function (event) {
+	if (event.which === 13) {
+		var todoText = $(this).val();
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+		$(this).val("");
+	}
+})
+
+$(".fa-pencil-square-o").click(400, function () {
+	$('input[type="text"]').fadeToggle();
+});
