ddb5ec1e4a83c7b00dfd87263a4c808798b36415
Ramachandra Junior <d.ajaykumar.017@gmail.com>
2016-08-29 12:05:10 +0530

    some changes

--- assets/js/todo-ext.js
+++ assets/js/todo-ext.js
@@ -1,13 +1,3 @@
-
-// Every time page reloads shit should be removed.
-// This early setting avoids unnecessary problems.
-var shitRemoved = false;
-// Just load all my cookie data into the todo list.
-// Whenever the script is loaded.
-getDataFromCookie();
-
-
-
 // Check off Specific Todos by clicking
 $("ul").on("click", "li", function () {
 	$(this).toggleClass("completed");
@@ -38,47 +28,4 @@ $('input[type="text"]').keypress(function (event) {
 $(".fa-pencil-square-o").click(400, function () {
 	$('input[type="text"]').fadeToggle();
 });
-
-
-
-// Create a function so that we can keep it running using setInterval
-// and keep track of all the inserted data.
-function putDataInCookie() {
-	// Select all li's.
-	var todoTasks = document.querySelectorAll("ul")[0].innerHTML.split(" ");
-	console.log(todoTasks);
-	/*
-	var todosAsString = "tasks=";
-
-	for (var i = 0; i < todoTasks.length; i++) {
-		// Add all the data to string and keep appending.
-		todosAsString += " &" + todoTasks[i].textContent;
-		console.log(todoTasks[i].innerHTML);
-	}
-	// todoAsString is absolutely fine.	
-	// Put that string in a cookie.	
-	document.cookie = todosAsString;
-	*/
-}
-
-function getDataFromCookie() {
-	// Get the cookie data.
-	var cookieData = document.cookie;
-	// Divide data into usable list.
-	var todosList = cookieData.split("&");
-	// Test if the useless data is removed.
-	// If not go on and remove.
-	if (shitRemoved === false) {
-		// Remove the first part of data as it is not useful.
-		todosList.shift();	
-		// Set dirt removed to true, so  that you wont chop off the real data.
-		shitRemoved = true;
-	}	
-
-	for (var i = 0; i < todosList.length; i++) {
-		$("ul").append(todosList[i]);
-	}
-}
-
-
 
