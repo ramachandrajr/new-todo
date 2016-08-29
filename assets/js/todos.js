a055e31d76f7bde8564f17b262e7d6fdc14164a0
Ramachandra Junior <d.ajaykumar.017@gmail.com>
2016-08-29 10:28:43 +0530

    some changes

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -23,3 +23,27 @@ $('input[type="text"]').keypress(function (event) {
 $(".fa-pencil-square-o").click(400, function () {
 	$('input[type="text"]').fadeToggle();
 });
+
+// Create a function so that we can keep it running using setInterval
+// and keep track of all the inserted data.
+function putDataInCookie() {
+	// Select all li's.
+	var todoTasks = document.querySelectorAll("li");
+	var todosAsString = "tasks=";
+
+	for (var i = 0; i < todoTasks.length; i++) {
+		// Add all the data to string and keep appending.
+		todosAsString += " &" + todoTasks[i].textContent;
+	}
+	// todoAsString is absolutely fine.
+
+	// Put that string in a cookie.
+	// document.cookie = todosAsString;
+	document.cookie = "userName= Ajay kumar";
+	var x = document.cookie;
+	console.log(x);
+}
+
+setInterval(putDataInCookie, 3000);
+// console.log(todosAsString.split(" & "));
+
